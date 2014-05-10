f2e4143a41eb5385832e2759e4f6d2bb4d3692b1
Ivo Nascimento <iannsp@gmail.com>
2014-05-10 18:33:55 -0300

    initial implementation of Item

---
+++ src/Ecomz/Cart/Item.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ecomz\Cart;
+
+class Item implements itemable
+{
+    private $config;
+    private function __construct(array $configItem)
+    {
+        $this->config = $configItem;
+    }
+    public static function factory(array $configItem)
+    {
+        return new self($configItem);
+    }
+    public function getId()
+    {
+        return $this->config['id'];
+    }
+
+    public function getProduct()
+    {
+        return $this->config['product'];
+    }
+}
